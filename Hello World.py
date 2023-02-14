42505934553a7307fc7487a0da8725c2e31ed8ff
Benjamin Terry <bterry308@gmail.com>
2023-02-13 20:29:06 -0600

    Practice Commit

--- Hello World.py
+++ Hello World.py
@@ -1,3 +1,5 @@
+# Generic Comment to practice git commits
+
 import os # os is auto included with python. Other modules will not be
 os.system('clear') # use whatever command is needed for that instance; gitbash uses "clear"
 
