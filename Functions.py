838077c9d65ef74168b78938f4c6ff6d0079d572
Benjamin Terry <bterry308@gmail.com>
2023-02-23 20:47:18 -0600

    Functions

---
+++ Functions.py
@@ -0,0 +1,17 @@
+import os # os is auto included with python. Other modules will not be
+os.system('clear') # use whatever command is needed for that instance; gitbash uses "clear"
+
+
+# tabs for spacing
+# Must be defined before use; best practice is to define all before everything else
+def nameit(first_name, last_name):
+    print(first_name,last_name) # space is auto included
+
+nameit("Ben","Terry")
+
+# Key word "return"
+def mathit(num1, num2):
+    return (num1+num2)
+
+test = mathit(2,5)
+print(test)
